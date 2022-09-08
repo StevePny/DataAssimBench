edfa4c239372116f6b765b3ee7dd585dde0f553f
Kylen Solvik <kysolvik@gmail.com>
2022-09-08 15:11:28 -0600

    Converted lorenz data object to fixture for tests

--- tests/lorenz63_class_test.py
+++ tests/lorenz63_class_test.py
@@ -3,21 +3,25 @@
 from dabench.data import data
 from dabench.data.lorenz63 import DataLorenz63
 import jax.numpy as jnp
+import pytest
 
 
-def test_initialization():
-    """Test the initialization size of class_data_Lorenz63 after generation."""
+@pytest.fixture
+def lorenz():
+    """Define Lorenz data object for rest of tests."""
     params = {'system_dimension': 3}
-    lorenz = DataLorenz63(**params)
+    return DataLorenz63(**params)
+
+
+def test_initialization(lorenz):
+    """Test the initialization size of class_data_Lorenz63 after generation."""
     lorenz.generate(t_final=1)
 
     assert len(lorenz.x0) == 3
 
 
-def test_variable_sizes():
+def test_variable_sizes(lorenz):
     """Test the variable sizes of class_data_Lorenz63."""
-    params = {'system_dimension': 3}
-    lorenz = DataLorenz63(**params)
     runtime = 1
     lorenz.generate(t_final=runtime)
 
@@ -25,10 +29,9 @@ def test_variable_sizes():
     assert lorenz.time_dim == runtime/lorenz.dt
 
 
-def test_trajectories_equal():
+def test_trajectories_equal(lorenz):
     """Test if two trajectories are the same with same initial conditions."""
     params = {'system_dimension': 3}
-    lorenz = DataLorenz63(**params)
     lorenz2 = DataLorenz63(**params)
     runtime = 1
     lorenz.generate(t_final=runtime)
@@ -37,13 +40,11 @@ def test_trajectories_equal():
     assert jnp.allclose(lorenz.values, lorenz2.values, rtol=1e-5, atol=0)
 
 
-def test_trajectory_shape():
+def test_trajectory_shape(lorenz):
     """Test output shape is (sys_dim, runtime)."""
-    params = {'system_dimension': 3}
-    lorenz = DataLorenz63(**params)
     # Typical Lorenz63 setup
     runtime = 1
-    sys_dim = 3 
+    sys_dim = 3
     lorenz.generate(t_final=runtime)
 
     assert lorenz.values.shape == (sys_dim, int(runtime/lorenz.dt))
