eb47b6e4c026510e41baabdadb42a539cf02e7d4
Kylen Solvik <kysolvik@gmail.com>
2022-09-12 08:13:29 -0600

    Tweaks to docstrings of tests

--- tests/lorenz63_class_test.py
+++ tests/lorenz63_class_test.py
@@ -8,20 +8,20 @@ import pytest
 
 @pytest.fixture
 def lorenz():
-    """Define Lorenz data object for rest of tests."""
+    """Defines class DataLorenz63 object for rest of tests."""
     params = {'system_dimension': 3}
     return DataLorenz63(**params)
 
 
 def test_initialization(lorenz):
-    """Test the initialization size of class_data_Lorenz63 after generation."""
+    """Tests the initialization size of class DataLorenz63 after generation."""
     lorenz.generate(t_final=1)
 
     assert len(lorenz.x0) == 3
 
 
 def test_variable_sizes(lorenz):
-    """Test the variable sizes of class_data_Lorenz63."""
+    """Test the variable sizes of class DataLorenz63."""
     runtime = 1
     lorenz.generate(t_final=runtime)
 
@@ -30,7 +30,7 @@ def test_variable_sizes(lorenz):
 
 
 def test_trajectories_equal(lorenz):
-    """Test if two trajectories are the same with same initial conditions."""
+    """Tests if two trajectories are the same with same initial conditions."""
     params = {'system_dimension': 3}
     lorenz2 = DataLorenz63(**params)
     runtime = 1
@@ -41,7 +41,7 @@ def test_trajectories_equal(lorenz):
 
 
 def test_trajectories_notequal(lorenz):
-    """Test if two trajectories are the different with different initial conditions."""
+    """Tests if two trajectories differ with different initial conditions."""
     params = {'system_dimension': 3}
     lorenz2 = DataLorenz63(**params)
     runtime = 1
@@ -53,7 +53,7 @@ def test_trajectories_notequal(lorenz):
 
 
 def test_trajectory_changes(lorenz):
-    """Test that last time step in trajectory is different from initial state"""
+    """Tests that last time step in trajectory is different from initial state"""
     runtime = 1
     lorenz.generate(t_final=runtime,
                     x0=jnp.array([-2.2, -2.2, 19.1]))
@@ -62,7 +62,7 @@ def test_trajectory_changes(lorenz):
 
 
 def test_trajectory_shape(lorenz):
-    """Test output shape is (runtime, sys_dim)."""
+    """Tests output shape is (runtime, sys_dim)."""
     # Typical Lorenz63 setup
     runtime = 1
     sys_dim = 3
