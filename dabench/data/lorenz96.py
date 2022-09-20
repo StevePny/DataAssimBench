430ae21bad3872a62b32ab53a55ebb11039c076d
Kylen Solvik <kysolvik@gmail.com>
2022-09-20 17:06:45 -0600

    Updated L96 defaults according to original paper

--- dabench/data/lorenz96.py
+++ dabench/data/lorenz96.py
@@ -1,35 +1,41 @@
-"""Lorenz 96 model"""
+"""Lorenz 96 model data generator"""
 import logging
 import jax.numpy as jnp
 
 from dabench.data import data
-from dabench.support.utils import integrate
 
 logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
 
 
 class DataLorenz96(data.Data):
-    """ Class to set up Lorenz 96 model data
+    """Class to set up Lorenz 96 model data.
+
+    Notes:
+        Default values come from Lorenz, 1996:
+        eapsweb.mit.edu/sites/default/files/Predicability_a_Problem_2006.pdf
 
     Attributes:
         forcing_term (float): Forcing constant for Lorenz96, prevents energy
-            from decaying to 0. Default is 8, from Lorenz, 1998.
-            https://doi.org/10.1175/1520-0469(1998)055<0399:OSFSWO>2.0.CO;2
+            from decaying to 0. Default is 8.0.
         x0 (ndarray, float): Initial state vector, array of floats of size
-            (system_dim). For system_dim of 5 or 6, defaults are set from a
-            20000 timestep spinup with delta_t=0.01. For system_dim > 6,
-            default is all 0s except the first element, which is set to 0.01.
+            (system_dim). For system_dim of 5, 6, or 36, defaults are the final
+            state of a 14400 timestep spinup with delta_t=0.05 starting with an
+            initial state (x0) of all 0s except the first element, which is set
+            to 0.01. This setup is taken from Lorenz, 1996. For all other
+            system_dim settings, default is all 0s except the first element,
+            which is set to 0.01.
         system_dim (int): System dimension, must be between 5 and 40.
-            Default is 5.
+            Default is 36.
         time_dim (int): Total time steps
-        delta_t (float): Length of one time step, default is 0.01.
+        delta_t (float): Length of one time step. Defaults is 0.05.
+            Lorenz, 1996.
     """
 
     def __init__(self,
                  forcing_term=8.,
-                 delta_t=0.01,
+                 delta_t=0.05,
                  x0=None,
-                 system_dim=3,
+                 system_dim=36,
                  time_dim=None,
                  values=None,
                  **kwargs):
@@ -47,16 +53,25 @@ class DataLorenz96(data.Data):
         self.forcing_term = forcing_term
         self.delta_t = delta_t
 
+        # Defaults come from 14400 timestep spinup with delta_t=0.05
+        initial_defaults_dict = {
+            5: jnp.array([3.552747, 5.173696, 5.4161925, 5.1463985,
+                          -0.62878036]),
+            6: jnp.array([-1.277197, 0.5191239, 2.1629903, 4.641832, 5.013882,
+                          -3.1474972]),
+            36: jnp.array([0.90061724,  2.2108543,  3.3563306,  7.0470520,
+                           7.3828993, -2.2906365,  1.6358340,  4.5246205,
+                           -0.8536633,  2.2018400,  2.5094680,  5.6148005,
+                           -1.7163916, -3.5827417,  0.22293478,  1.8138107,
+                           3.7354333,  5.9006715, -4.6722836,  0.4664867,
+                           0.36800075,  7.7004447,  3.0569422, -1.7238870,
+                           -2.1296368,  1.6388168,  5.1955190,  4.7863874,
+                           0.8382774, -4.0938597,  0.5181451,  1.2503184,
+                           6.0076460,  7.1161866, -3.2190716, -2.3532054])
+            }
         # Set initial conditions
         if x0 is None:
-            if system_dim <= 6:
-                # Defaults come from 20000 timestep spinup with delta_t=0.01
-                initial_defaults_dict = {
-                    5: jnp.array([7.97355787, 7.97897913, 8.00370696, 
-                                  7.98444298, 7.97446945]),
-                    6: jnp.array([1.50995768, 1.56066941, 4.25604607,
-                                  7.25271592, -3.11392061, 2.26497509])
-                    }
+            if system_dim in initial_defaults_dict.keys():
                 x0 = initial_defaults_dict[system_dim]
             else:
                 # Set to all 0s except first element = 0.01
