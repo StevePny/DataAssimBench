b066178835a5eebdbed7e2b211e003eeff2fbb9e
Kylen Solvik <kysolvik@gmail.com>
2022-09-21 09:55:30 -0600

    return_tlm code added

--- dabench/data/data.py
+++ dabench/data/data.py
@@ -72,25 +72,35 @@ class Data():
                                i in range(len(self.original_dim) + 1))
         return self.to_original_dim()[tupled_targets]
 
-    def generate(self, n_steps=None, t_final=None, x0=None, stride=None,
-                 **kwargs):
+    def generate(self, n_steps=None, t_final=None, x0=None, M0=None,
+                 return_tlm=False, stride=None, **kwargs):
         """Generates a dataset and assigns values and times to the data object.
 
         Notes:
             Either provide n_steps or t_final in order to indicate the length
             of the forecast. These are used to set the values, times, and
-            time_dim attributes
+            time_dim attributes.
 
         Args:
             n_steps (int): Number of timesteps. One of n_steps OR
                 t_final must be specified.
             t_final (float): Final time of trajectory. One of n_steps OR
                 t_final must be specified.
+            M0 (ndarray): the initial condition of the TLM matrix computation
+                shape (system_dim, system_dim).
+            return_tlm (bool): specifies whether to compute and return the
+                integrated Jacobian as a Tangent Linear Model for each
+                timestep.
             x0 (ndarray): initial conditions state vector of shape (system_dim)
-            stride (int): specify how many steps to skip in the output data 
-                versus the model timestep (delta_t)
+            stride (int): specify how many steps to skip in the output data
+                versus the model timestep (delta_t).
             **kwargs: arguments to the integrate function (permits changes in
-                convergence tolerance, etc.)
+                convergence tolerance, etc.).
+
+        Returns:
+            Nothing if return_tlm=False. If return_tlm=True, returns tuple
+                (xaux, M) where xaux is the system trajectory and M is a list
+                of TLMs corresponding to the system trajectory.
         """
 
         # Check that n_steps or t_final is supplied
@@ -108,10 +118,22 @@ class Data():
             else:
                 raise Exception('Initial condition is None,x0 = {}), it must either be provided as an argument or set as an attribute in the model object.'.format(x0))
 
-        # Check that self.rhs is defined in model object
-        if self.rhs is None:
-            raise Exception('self.rhs must be specified prior to calling generate.')
-        f = self.rhs
+        # Check that self.rhs or self.rhs_aux are defined in model object
+        if return_tlm:
+            # Prep x0
+            if M0 is None:
+                M0 = jnp.identity(self.system_dim)
+            xaux0 = jnp.concatenate((x0.ravel(), M0.ravel()))
+            x0 = xaux0
+            # Check that self.rhs_aux is defined
+            if self.rhs_aux is None:
+                raise Exception('self.rhs_aux must be specified prior to calling generate.')
+            f = self.rhs_aux
+        else:
+            # Check that self.rhs is defined
+            if self.rhs is None:
+                raise Exception('self.rhs must be specified prior to calling generate.')
+            f = self.rhs
 
         # Integrate and store values and times
         # If data object has its own integration method, use that
@@ -129,3 +151,41 @@ class Data():
         self.values = y[:, :self.system_dim]
         self.times = t
         self.time_dim = len(t)
+
+        # Return the data series and associated TLMs if requested
+        if return_tlm:
+            # Reshape output
+            xaux = y[:, :self.system_dim]
+
+            # Reshape M matrix
+            M = jnp.reshape(y[:, self.system_dim:],
+                            (self.time_dim,
+                             self.system_dim,
+                             self.system_dim)
+                            )
+
+            return xaux, M
+
+    def rhs_aux(self, x, t):
+        """The auxiliary model used to compute the TLM.
+
+        Args:
+          x (ndarray): State vector with size (system_dim)
+          t (ndarray): Array of times with size (time_dim)
+
+        Returns:
+          dxaux (ndarray): State vector [size: (system_dim,)]
+
+        """
+        # Compute M
+        dxdt = self.rhs(x[:self.system_dim], t)
+        J = self.Jacobian(x[:self.system_dim])
+        M = jnp.array(jnp.reshape(x[self.system_dim:], (self.system_dim,
+                                                        self.system_dim)))
+
+        # Matrix multiplication
+        dM = J @ M
+
+        dxaux = jnp.concatenate((dxdt, dM.flatten()))
+
+        return dxaux
