1e085e2e9ab956a14c879cbb1df9ff12c61af03d
Kylen Solvik <kysolvik@gmail.com>
2022-11-07 17:19:32 -0700

    aws ERA5 method, but not working - loading values takes too long

---
+++ dabench/data/aws.py
@@ -0,0 +1,57 @@
+"""Load data from AWS Registry of Open Data
+
+For now just ERA5 ECMWF data:
+https://registry.opendata.aws/ecmwf-era5/
+"""
+
+
+import xarray as xr
+from dabench.data import data
+import fsspec
+
+
+class DataAWS(data.Data):
+    """Class for loading data from AWS Open Data"""
+
+    def __init__(
+            self,
+            variables=['air_temperature_at_2_metres'],
+            months=['01', '02', '03', '04', '05', '06',
+                    '07', '08', '09', '10', '11', '12'],
+            years=[2020],
+            system_dim=None,
+            time_dim=None,
+            **kwargs
+            ):
+        self.variables = variables
+        self.months = months
+        self.years = years
+
+
+        super().__init__(system_dim=system_dim, time_dim=time_dim,
+                         values=None, delta_t=None, **kwargs)
+
+    def _build_urls(self):
+        
+        file_pattern = 'http://era5-pds.s3.amazonaws.com/{year}/{month}/data/{variable}.nc'
+        urls_mapper = [file_pattern.format(year=y, month=m, variable=v)
+                       for y in self.years
+                       for m in self.months
+                       for v in self.variables
+                       ]
+
+        return urls_mapper
+
+    def _get_filehandles(self, urls_mapper):
+        return [fsspec.open(url).open() for url in urls_mapper]
+
+    def load_aws_era5(self):
+
+        urls_mapper = self._build_urls()
+        filehandles = self._get_filehandles(urls_mapper)
+
+        ds = xr.open_mfdataset(filehandles)
+
+        self._import_xarray_ds(ds)
+
+
