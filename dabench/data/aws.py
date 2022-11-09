202165ca4a1b09f3097222e92b98aff8750467c6
Kylen Solvik <kysolvik@gmail.com>
2022-11-09 14:43:53 -0700

    Subset aws era5 zarr by lat/lon bounding box, otherwise too much data

--- dabench/data/aws.py
+++ dabench/data/aws.py
@@ -19,6 +19,11 @@ class DataAWS(data.Data):
             months=['01', '02', '03', '04', '05', '06',
                     '07', '08', '09', '10', '11', '12'],
             years=[2020],
+            # Defaults are Cuba bounding box
+            min_lat=19.8554808619,
+            max_lat=23.1886107447,
+            min_lon=-84.9749110583,
+            max_lon=-74.1780248685,
             system_dim=None,
             time_dim=None,
             **kwargs
@@ -26,6 +31,10 @@ class DataAWS(data.Data):
         self.variables = variables
         self.months = months
         self.years = years
+        self.min_lat = min_lat
+        self.max_lat = max_lat
+        self.min_lon = min_lon
+        self.max_lon = max_lon
 
         super().__init__(system_dim=system_dim, time_dim=time_dim,
                          values=None, delta_t=None, **kwargs)
@@ -49,6 +58,19 @@ class DataAWS(data.Data):
                                concat_dim='time0', combine='nested',
                                coords='minimal', compat='override',
                                parallel=True)
+        if self.min_lat is not None and self.max_lat is not None:
+            # Subset by lat boundaries
+            ds = ds.sel(lat=slice(self.max_lat, self.min_lat))
+        if self.min_lon is not None and self.max_lon is not None:
+            # Convert west longs to degrees east
+            subset_min_lon = self.min_lon
+            subset_max_lon = self.max_lon
+            if subset_min_lon < 0:
+                subset_min_lon += 360
+            if subset_max_lon < 0:
+                subset_max_lon += 360
+            # Subset by lon boundaries
+            ds = ds.sel(lon=slice(subset_min_lon, subset_max_lon))
 
         self._import_xarray_ds(ds)
 
