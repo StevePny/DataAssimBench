9cfc9885cfee46bcc2b256c4ac6955188c26fb76
Kylen Solvik <kysolvik@gmail.com>
2022-11-22 08:51:06 -0700

    Updated inits for better imports

---
+++ dabench/data/__init__.py
@@ -0,0 +1,10 @@
+from .data import Data
+
+from .lorenz63 import DataLorenz63
+from .lorenz96 import DataLorenz96
+from .sqgturb import DataSQGturb
+from .aws import DataAWS
+from .gcp import DataGCP
+from .pyqg import DataPYQG
+from .barotropic import DataBarotropic
+from .enso_idx import DataENSOIDX
