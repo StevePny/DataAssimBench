c929b8952960fdf9f8f84ce052c67af79c326d2a
Kylen Solvik <kysolvik@gmail.com>
2022-11-30 17:25:46 -0700

    Rename PYQG class to PyQG

--- tests/data_pyqg_test.py
+++ tests/data_pyqg_test.py
@@ -1,26 +1,26 @@
-"""Tests for PYQG class (dabench.data.pyqg)"""
+"""Tests for PyQG class (dabench.data.pyqg)"""
 
 import numpy as np
 import pytest
 
-from dabench.data import PYQG
+from dabench.data import PyQG
 
 
 @pytest.fixture
 def pyqg():
     """Defines class PYQ object for rest of tests."""
-    pyqg_obj = PYQG()
+    pyqg_obj = PyQG()
     pyqg_obj.generate(n_steps=1000)
     return pyqg_obj
 
 
 def test_initialization(pyqg):
-    """Tests the initialization size of class PYQG after generation."""
+    """Tests the initialization size of class PyQG after generation."""
     assert pyqg.x0.shape == (2, 64, 64)
 
 
 def test_variable_sizes(pyqg):
-    """Test the variable sizes of class PYQG."""
+    """Test the variable sizes of class PyQG."""
     assert pyqg.system_dim == 8192
     assert pyqg.time_dim == 1000
     assert pyqg.original_dim == (2, 64, 64)
@@ -32,14 +32,14 @@ def test_to_original_dim(pyqg):
 
 def test_trajectories_equal(pyqg):
     """Tests if two trajectories are the same with same initial conditions."""
-    pyqg2 = PYQG()
+    pyqg2 = PyQG()
     pyqg2.generate(n_steps=1000)
     assert np.allclose(pyqg.values, pyqg2.values, rtol=1e-5, atol=0)
 
 
 def test_trajectories_notequal_diffparams(pyqg):
     """Tests if two trajectories differ with different params."""
-    pyqg2 = PYQG(rd=10000, H1=250)
+    pyqg2 = PyQG(rd=10000, H1=250)
     pyqg2.generate(n_steps=1000)
     assert not np.allclose(pyqg.values, pyqg2.values, rtol=1e-5, atol=0)
 
@@ -47,7 +47,7 @@ def test_trajectories_notequal_diffparams(pyqg):
 def test_trajectories_notequal_diffic(pyqg):
     """Tests if two trajectories differ with different initial conditions."""
     x0 = pyqg.x0 + 0.01
-    pyqg2 = PYQG(x0=x0)
+    pyqg2 = PyQG(x0=x0)
     pyqg2.generate(n_steps=1000)
     assert not np.allclose(pyqg.values, pyqg2.values, rtol=1e-5, atol=0)
 
