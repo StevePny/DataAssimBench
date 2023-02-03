13ad64ddbd52bacf8ecf9101540d2bbc17040b52
Kylen Solvik <kysolvik@gmail.com>
2023-02-03 12:09:31 -0700

    Add ObsVector to init

--- dabench/vector/__init__.py
+++ dabench/vector/__init__.py
@@ -1,8 +1,9 @@
 from ._vector import _Vector
 
-
 from ._state_vector import StateVector
+from ._obs_vector import ObsVector
 
 __all__ = [
-    'StateVector'
+    'StateVector',
+    'ObsVector'
     ]
