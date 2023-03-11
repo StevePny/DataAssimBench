5b8c2b2a97b4cfc89c3af7bddfbf3714d949b10b
Kylen Solvik <kysolvik@gmail.com>
2023-03-10 17:44:30 -0700

    Allowing for speicfying number forrandom location and time indices

--- dabench/observer/_observer.py
+++ dabench/observer/_observer.py
@@ -14,106 +14,171 @@ class Observer():
     Attributes:
         data_obj (dabench.data.Data): Data generator/loader object from which
             to gather observations.
-        location_indices (ndarray): Indices to gather observations
-            from. If 1D array provided, assumed to be for flattened system_dim.
+        random_location_density (float or tuple): Fraction of locations in
+            system_dim to randomly select for observing, must be value
+            between 0 and 1. Default is 1.
+        random_time_density (float): Fraction of times to randomly select
+            for observing must be value between 0 and 1. Default is 1.
+        random_location_count (int): Number of locations in data_obj's
+            system_dim to randomly select for observing. Default is None.
+            User should specify one of: random_location_count,
+            random_location_density, or location_indices.
+            If random_location_count is specified, it takes precedent over
+            random_location_density.
+        random_time_count (int): Number of times to randomly select for
+            observing. Default is None. User should specify one of:
+            random_time_count, random_time_density, or time_indices. 
+            If random_time_count is specified, it takes precedent over
+            random_time_density.
+        location_indices (ndarray): Manually specified indices for observing.
+            If 1D array provided, assumed to be for flattened system_dim.
             If >1D, must have same dimensionality as data generator's
             original_dim (e.g. (x, y, z)). If stationary_observers=False,
             expects leading time dimension. If not specified, will be randomly
-            generated according to location_density. Default is None.
-        location_density (float or tuple): Fraction(s) of locations to gather
-            observations from, must be values between 0 and 1. If one value is
-            provided, samples from flattened system_dim. If tuple/list-like of
-            values, length must match dimensionality of data generator's
-            original_dim and each value is used to sample from its respective
-            dimension.
-        stationary_observers (bool): If True,
-            samples from same indices at each time step. If False, randomly
-            generates/expects new observation indices at each timestep.
-            If False:
-                If using location_density, indices are randomly generated.
-                If using location_indices, expects indices to either be 2D
-                (time_dim, system_dim) or >2D (time_dim, original_dim).
-                Default is True .
+            generated according to random_location_density OR
+            random_location_count. Default is None.
         time_indices (ndarray): Indices of times to gather observations from.
-            If not specified, randomly generate according to time_density.
-            Default is None.
-        time_density (float): Fraction of times to gather observations from,
-            must be value between 0 and 1. Default is 1.
+            If not specified, randomly generate according to
+            random_time_density OR random_time_count. Default is None.
+        stationary_observers (bool): If True, samples from same indices at 
+            each time step. If False, randomly generates/expects new 
+            observation indices at each timestep. Default is True.
+            If False:
+                If using random_location_count, the same number of indices
+                    will be randomly generated. 
+                If using random_location_density, indices are randomly
+                    generated, with the possibility of a different number
+                    of locations at each times step..
+                If using location_indices, expects indices to either be 2D
+                    (time_dim, system_dim) or >2D (time_dim, original_dim).
         error_bias (float): Mean of normal distribution of observation errors.
             Default is 0.
         error_sd (float): Standard deviation of observation errors. Default is
             0.
         error_positive_only (bool): Clip errors to be positive only. Default is
             False.
-        random_seed (int): Random seed for sampling times and locations
-            according to location_density and time_density. Default is 99.
+        random_seed (int): Random seed for sampling times and locations.
+            Default is 99.
     """
 
     def __init__(self,
                  data_obj,
-                 location_indices=None,
-                 location_density=1.,
-                 stationary_observers=True,
+                 random_time_density=1.,
+                 random_location_density=1.,
+                 random_time_count=None,
+                 random_location_count=None,
                  time_indices=None,
-                 time_density=1.,
+                 location_indices=None,
+                 stationary_observers=True,
                  error_bias=0.,
                  error_sd=0.,
                  error_positive_only=False,
                  random_seed=99,
                  ):
+
         self.data_obj = data_obj
-        if location_indices is not None:
-            location_indices = np.array(location_indices)
-        self.location_indices = location_indices
-        self.location_density = location_density
-        self.stationary_observers = stationary_observers
+
         if time_indices is not None:
             time_indices = np.array(time_indices)
         self.time_indices = time_indices
-        self.time_density = time_density
+        self.random_time_density = random_time_density
+        self.random_time_count = random_time_count
+
+        if location_indices is not None:
+            location_indices = np.array(location_indices)
+        self.location_indices = location_indices
+        self.random_location_density = random_location_density
+        self.random_location_count = random_location_count
+        self.stationary_observers = stationary_observers
+
         self.error_bias = error_bias
         self.error_sd = error_sd
         self.error_positive_only = error_positive_only
         self.random_seed = random_seed
 
     def _generate_time_indices(self, rng):
-        self.time_indices = np.where(
-                rng.binomial(1, p=self.time_density,
-                             size=self.data_obj.time_dim
-                             ).astype('bool')
-                )[0]
+        if self.random_time_count is not None:
+            self.time_indices = rng.choice(
+                    self.data_obj.time_dim,
+                    size=self.random_time_count,
+                    replace=False,
+                    shuffle=False)
+        else:
+            self.time_indices = np.where(
+                    rng.binomial(1, p=self.random_time_density,
+                                 size=self.data_obj.time_dim
+                                 ).astype('bool')
+                    )[0]
 
     def _generate_stationary_indices(self, rng):
-        self.location_indices = np.where(
-                rng.binomial(1, p=self.location_density,
-                             size=self.data_obj.system_dim
-                             ).astype('bool')
-                )[0]
+        if self.random_location_count is not None:
+            self.location_indices = rng.choice(
+                    self.data_obj.system_dim,
+                    size=self.random_location_count,
+                    replace=False,
+                    shuffle=False)
+        else:
+            self.location_indices = np.where(
+                    rng.binomial(1, p=self.random_location_density,
+                                 size=self.data_obj.system_dim
+                                 ).astype('bool')
+                    )[0]
 
     def _generate_nonstationary_indices(self, rng):
-        self.location_indices = np.array([
-                np.where(
-                    rng.binomial(1, p=self.location_density,
-                                 size=self.data_obj.system_dim
-                                 ).astype('bool'))[0]
-                for i in range(self.time_indices.shape[0])
-                ], dtype=object)
+        if self.random_location_count is not None:
+            self.location_indices = np.array([
+                rng.choice(
+                    self.data_obj.system_dim,
+                    size=self.random_location_count,
+                    replace=False,
+                    shuffle=False)
+                for i in range(self.time_indices.shape[0]))
+        else:
+            self.location_indices = np.array([
+                    np.where(
+                        rng.binomial(1, p=self.random_location_density,
+                                     size=self.data_obj.system_dim
+                                     ).astype('bool'))[0]
+                    for i in range(self.time_indices.shape[0])
+                    ], dtype=object)
 
     def _generate_stationary_indices_gridded(self, rng):
-        self.location_indices = np.array(np.where(
-                rng.binomial(1, p=self.location_density,
-                             size=self.data_obj.original_dim
-                             ).astype('bool')
-                )).T
+        if self.random_location_count is not None:
+            arange_list = [np.arange(n) for n in self.data_obj.original_dim]
+            ind_possibilities = np.array(
+                np.meshgrid(*arange_list).T.reshape(
+                    -1, len(self.data_obj.original_dim)
+            self.location_indices = rng.choice(
+                    ind_possibilities,
+                    size=self.random_location_count,
+                    replace=False,
+                    shuffle=False)
+        else:
+            self.location_indices = np.array(np.where(
+                    rng.binomial(1, p=self.random_location_density,
+                                 size=self.data_obj.original_dim
+                                 ).astype('bool')
+                    )).T
 
     def _generate_nonstationary_indices_gridded(self, rng):
-        self.location_indices = np.array([
-                np.array(np.where(
-                    rng.binomial(1, p=self.location_density,
-                                 size=self.data_obj.original_dim
-                                 ).astype('bool'))).T
-                for i in range(self.time_indices.shape[0])
-                ], dtype=object)
+        if self.random_location_count is not None:
+            arange_list = [np.arange(n) for n in self.data_obj.original_dim]
+            ind_possibilities = np.array(
+                np.meshgrid(*arange_list).T.reshape(
+                    -1, len(self.data_obj.original_dim)
+            self.location_indices = np.array([rng.choice(
+                    ind_possibilities,
+                    size=self.random_location_count,
+                    replace=False,
+                    shuffle=False) for i in range(self.time_indices.shape[0])
+        else:
+            self.location_indices = np.array([
+                    np.array(np.where(
+                        rng.binomial(1, p=self.random_location_density,
+                                     size=self.data_obj.original_dim
+                                     ).astype('bool'))).T
+                    for i in range(self.time_indices.shape[0])
+                    ], dtype=object)
 
     def _sample_stationary(self, errors_vector, sample_in_system_dim):
         if sample_in_system_dim:
