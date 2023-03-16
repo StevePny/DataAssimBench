0c62135ba7ef318e7ff90b43b567626a24e11f46
Kylen Solvik <kysolvik@gmail.com>
2023-03-16 17:23:34 -0600

    Updated obsop tests

--- tests/obsop_base_test.py
+++ tests/obsop_base_test.py
@@ -46,24 +46,39 @@ def obsop_default():
     return obsop.ObsOp()
 
 
-@pytest.fixture
-def obsop_manual_locindices():
-    """Defines ObsOp with manually specified loc indices"""
-    return obsop.ObsOp(location_indices=[0])
+def custom_h(state_vec, obs_vec):
+    """Custom h that sums the values at obs_vec.location_indices"""
+    out_vals = [[state_vec.values[i][np.array(obs_vec.location_indices)].sum()]
+                for i in range(state_vec.time_dim)]
+    return vector.ObsVector(
+                values=out_vals,
+                times=state_vec.times,
+                store_as_jax=state_vec.store_as_jax
+                )
 
-
 @pytest.fixture
-def obsop_custom_H():
-    """Defines ObsOp with custom H operator"""
+def obsop_custom_h():
+    """Defines ObsOp with custom h operator"""
     return obsop.ObsOp(
-            H=np.array([[5., 2.], [0., 1.]])
+            h=custom_h
             )
+
+@pytest.fixture
+def obsvector_locindices_1():
+    """Defines ObsVector with location_indices for other tests"""
+    return vector.ObsVector(location_indices=[0])
+
+
+@pytest.fixture
+def obsvector_locindices_2():
+    """Defines ObsVector with location_indices for other tests"""
+    return vector.ObsVector(location_indices=[0, 1])
 
 
 def test_defaults_singlesv(single_sv, obsop_default):
     """Test default settings on single timestep"""
 
-    obs_vec = obsop_default.observe(single_sv)
+    obs_vec = obsop_default.h(single_sv)
 
     assert obs_vec.obs_dims == np.array([2])
     assert obs_vec.time_dim == 1
@@ -74,7 +89,7 @@ def test_defaults_singlesv(single_sv, obsop_default):
 
 
 def test_defaults_singlesv_jax(single_sv_jax, obsop_default):
-    obs_vec = obsop_default.observe(single_sv_jax)
+    obs_vec = obsop_default.h(single_sv_jax)
 
     assert obs_vec.obs_dims == np.array([2])
     assert obs_vec.time_dim == 1
@@ -88,7 +103,7 @@ def test_defaults_singlesv_jax(single_sv_jax, obsop_default):
 def test_defaults_trajectory(sv_trajectory, obsop_default):
     """Test default settings on trajectory (multiple timesteps)"""
 
-    obs_vec = obsop_default.observe(sv_trajectory)
+    obs_vec = obsop_default.h(sv_trajectory)
 
     assert np.array_equal(obs_vec.obs_dims, np.repeat(3, 8))
     assert obs_vec.time_dim == 8
@@ -98,23 +113,27 @@ def test_defaults_trajectory(sv_trajectory, obsop_default):
     assert isinstance(obs_vec.values, np.ndarray)
 
 
-def test_man_locinds_singlesv(single_sv, obsop_manual_locindices):
+def test_man_locinds_singlesv(single_sv,
+                              obsop_default,
+                              obsvector_locindices_1):
     """Test manually specified indices on single timestep"""
 
-    obs_vec = obsop_manual_locindices.observe(single_sv)
+    obs_vec = obsop_default.h(single_sv, obsvector_locindices_1)
 
     assert obs_vec.obs_dims == np.array([1])
     assert obs_vec.time_dim == 1
     assert np.array_equal(obs_vec.times, np.array([0]))
     assert np.array_equal(obs_vec.location_indices, np.array([0]))
-    assert np.array_equal(obs_vec.values, np.array([[1.]]))
+    assert np.array_equal(obs_vec.values, np.array([[1]]))
     assert isinstance(obs_vec.values, np.ndarray)
 
 
-def test_man_locinds_trajectory(sv_trajectory, obsop_manual_locindices):
+def test_man_locinds_trajectory(sv_trajectory,
+                                obsop_default,
+                                obsvector_locindices_1):
     """Test manually specified indices on trajectory"""
 
-    obs_vec = obsop_manual_locindices.observe(sv_trajectory)
+    obs_vec = obsop_default.h(sv_trajectory, obsvector_locindices_1)
 
     assert np.array_equal(obs_vec.obs_dims, np.repeat(1, 8))
     assert obs_vec.time_dim == 8
@@ -125,13 +144,29 @@ def test_man_locinds_trajectory(sv_trajectory, obsop_manual_locindices):
     assert isinstance(obs_vec.values, np.ndarray)
 
 
-def test_custom_H_singlesv(single_sv, obsop_custom_H):
+def test_custom_h_singlesv(single_sv, obsop_custom_h, obsvector_locindices_2):
     """Test default settings on single timestep"""
 
-    obs_vec = obsop_custom_H.observe(single_sv)
+    obs_vec = obsop_custom_h.h(single_sv, obsvector_locindices_2)
 
-    assert obs_vec.obs_dims == np.array([2]) 
+    assert obs_vec.obs_dims == np.array([1])
     assert obs_vec.time_dim == 1
     assert np.array_equal(obs_vec.times, np.array([0]))
-    assert np.array_equal(obs_vec.values, np.array([[9., 2.]]))
+    assert np.array_equal(obs_vec.values, np.array([[3]]))
+    assert isinstance(obs_vec.values, np.ndarray)
+
+
+def test_custom_h_trajectory(sv_trajectory,
+                             obsop_custom_h,
+                             obsvector_locindices_2):
+    """Test default settings on single timestep"""
+
+    obs_vec = obsop_custom_h.h(sv_trajectory, obsvector_locindices_2)
+
+    assert np.array_equal(obs_vec.obs_dims, np.repeat(1, 8))
+    assert obs_vec.time_dim == 8
+    assert np.array_equal(obs_vec.times, np.arange(8))
+    assert np.array_equal(obs_vec.values,
+                          np.array([[1], [7], [13], [19], [25], [31], [37],
+                                    [43]]))
     assert isinstance(obs_vec.values, np.ndarray)
