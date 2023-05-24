a3d5e342311ec38a0e19c1598ec3ef35fec4cdbf
Kylen Solvik <kysolvik@gmail.com>
2023-05-24 17:45:44 -0600

    Add store_as_jax option to observer

--- dabench/observer/_observer.py
+++ dabench/observer/_observer.py
@@ -59,6 +59,9 @@ class Observer():
             False.
         random_seed (int): Random seed for sampling times and locations.
             Default is 99.
+        store_as_jax (bool): Store values as jax array instead of numpy array.
+            Default is False (store as numpy).
+
     """
 
     def __init__(self,
@@ -74,6 +77,7 @@ class Observer():
                  error_sd=0.,
                  error_positive_only=False,
                  random_seed=99,
+                 store_as_jax=False,
                  ):
 
         self.data_obj = data_obj
@@ -95,6 +99,7 @@ class Observer():
         self.error_sd = error_sd
         self.error_positive_only = error_positive_only
         self.random_seed = random_seed
+        self.store_as_jax = store_as_jax
 
     def _generate_time_indices(self, rng):
         if self.random_time_count is not None:
@@ -328,5 +333,6 @@ class Observer():
                          errors=errors_vector,
                          error_dist='normal',
                          error_sd=self.error_sd,
-                         error_bias=self.error_bias
+                         error_bias=self.error_bias,
+                         store_as_jax=self.store_as_jax,
                          )
