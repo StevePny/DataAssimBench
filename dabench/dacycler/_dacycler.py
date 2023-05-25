8c34cfa0c3a988e3090dd4a48551a91c129f09af
Kylen Solvik <kysolvik@gmail.com>
2023-05-25 15:30:03 -0600

    Cleaned up dacycler base class, added documentation

--- dabench/dacycler/_dacycler.py
+++ dabench/dacycler/_dacycler.py
@@ -9,39 +9,36 @@ class DACycler():
 
     Attributes:
         system_dim (int): system dimension
-        time_dim (int): total time steps
         delta_t (float): the timestep of the model (assumed uniform)
-        model_obj (obj): underlying model object, e.g. pytorch neural network.
+        forecast_model (obj): underlying model object, e.g.
+            pytorch neural network.
+        in_4d (bool): True for 4D data assimilation techniques (e.g. 4DVar).
+            Default is False.
+        ensemble (bool): True for ensemble-based data assimilation techniques (ETKF).
+            Default is False
     """
     def __init__(self,
                  system_dim=None,
                  delta_t=None,
-                 start_time=0,
-                 end_time=None,
-                 num_cycles=1,
-                 window_time=None,
+                 forecast_model=None,
                  in_4d=False,
                  ensemble=False,
-                 analysis_window=None,
-                 observation_window=None,
-                 observations=None,
-                 forecast_model=None,
-                 truth_obj=None,
                  **kwargs
                  ):
 
+        self.in_4d = in_4d
         self.ensemble = ensemble
         self.system_dim = system_dim
         self.delta_t = delta_t
         self.forecast_model = forecast_model
-        self.truth_obj = truth_obj
 
     def cycle(self,
               input_state,
               start_time,
               obs_vector,
               timesteps,
-              obs_time_window=0.2):
+              analysis_window,
+              analysis_time_in_window=None):
         """Perform DA cycle repeatedly, including analysis and forecast
 
         Args:
@@ -49,23 +46,29 @@ class DACycler():
             obs_vector (vector.ObsVector): Observations vector.
             start_time (float or datetime-like): Starting time.
             timesteps (int): Number of timesteps, in model time.
-            obs_time_window (float): Time window from which to gather
-                observations for DA Cycle. Takes observations that are +/-
-                obs_time_window from time of each analysis step.
+            analysis_window (float): Time window from which to gather
+                observations for DA Cycle.
+            analysis_time_in_window (float): Where within analysis_window
+                to perform analysis. For example, 0.0 is the start of the
+                window. Default is None, which selects the middle of the
+                window.
 
         Returns:
             vector.StateVector of analyses and times.
         """
+
+        if analysis_time_in_window is None:
+            analysis_time_in_window = analysis_window/2
         # For storing outputs
         all_analyses = []
         all_times = []
-        cur_time = start_time
+        cur_time = start_time + analysis_time_in_window
         cur_state = input_state
 
         for i in range(timesteps):
             # 1. Filter observations to plus/minus 0.1 from that time
             obs_vec_timefilt = obs_vector.filter_times(
-                cur_time - obs_time_window, cur_time + obs_time_window)
+                cur_time - analysis_window/2, cur_time + analysis_window/2)
 
             if obs_vec_timefilt.values.shape[0] > 0:
                 # 2. Calculate analysis
