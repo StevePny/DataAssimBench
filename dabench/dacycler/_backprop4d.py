564c28bc7795bee8659d4a7c1b63650e34045775
Kylen Solvik <kysolvik@gmail.com>
2023-07-28 11:43:38 -0300

    Fully jaxified backprop4d, MUCH faster (200 10-step cycles in under a minute depending on num_epochs)

--- dabench/dacycler/_backprop4d.py
+++ dabench/dacycler/_backprop4d.py
@@ -5,7 +5,8 @@ import inspect
 import numpy as np
 import jax.numpy as jnp
 import jax.scipy as jscipy
-from jax import value_and_grad
+from jax import value_and_grad, grad
+import jax
 
 from dabench import dacycler, vector
 
@@ -24,6 +25,8 @@ class Backprop4D(dacycler.DACycler):
                  H=None,
                  learning_rate=1e-5,
                  num_epochs=20,
+                 steps_per_window=1,
+                 obs_window_indices=[0],
                  **kwargs
                  ):
 
@@ -33,48 +36,23 @@ class Backprop4D(dacycler.DACycler):
         self.B = B
         self.num_epochs = num_epochs
         self.learning_rate = learning_rate
+        self.steps_per_window = steps_per_window
+        self.obs_window_indices = obs_window_indices
 
         super().__init__(system_dim=system_dim,
                          delta_t=delta_t,
                          model_obj=model_obj)
 
-    def step_cycle(self, xb, yo, H=None, h=None, R=None, B=None, n_steps=1,
-                   obs_window_indices=[0]):
-        """Perform one step of DA Cycle
-
-        Args:
-            xb:
-            yo:
-            H
-
-
-        Returns:
-            vector.StateVector containing analysis results
-
-        """
-        time_sel_matrix = self._calc_time_sel_matrix(obs_window_indices,
-                                                     n_steps)
-        if H is not None or h is None:
-            return self._cycle_linear_obsop(xb, yo, H, R, B, n_steps=n_steps,
-                                            time_sel_matrix=time_sel_matrix)
-        else:
-            return self._cycle_general_obsop(xb, yo, h, R, B, n_steps=n_steps,
-                                             time_sel_matrix=time_sel_matrix)
-
-    def _calc_default_H(self, obs_vec):
-        """If H is not provided, creates identity matrix to serve as H"""
-        H = jnp.zeros((obs_vec.values.flatten().shape[0], self.system_dim))
-        H = H.at[jnp.arange(H.shape[0]), obs_vec.location_indices.flatten()
+    def _calc_default_H(self, obs_values, obs_loc_indices):
+        H = jnp.zeros((obs_values.flatten().shape[0], self.system_dim))
+        H = H.at[jnp.arange(H.shape[0]), obs_loc_indices.flatten()
                  ].set(1)
         return H
 
-    def _calc_default_R(self, obs_vec):
-        """If R i s not provided, calculates default based on observation error"""
-        return jnp.identity(obs_vec.values.flatten().shape[0])*obs_vec.error_sd**2
+    def _calc_default_R(self, obs_values, obs_error_sd):
+        return jnp.identity(obs_values.flatten().shape[0])*(obs_error_sd**2)
 
     def _calc_default_B(self):
-        """If B is not provided, identity matrix with shape (system_dim, system_dim."""
-
         return jnp.identity(self.system_dim)
 
     def _make_loss(self, obs_vals, H, B, R, time_sel_matrix, rb0, n_steps):
@@ -110,20 +88,28 @@ class Backprop4D(dacycler.DACycler):
                 jnp.arange(time_sel_matrix.shape[0]), obs_steps_inds].set(1)
         return time_sel_matrix
 
-    def _cycle_general_obsop(self, forecast, obs_vec):
-        return
+    def _make_backprop_epoch(self, loss_func):
 
-    def _cycle_linear_obsop(self, forecast, obs_vec, H=None, R=None,
-                            B=None, time_sel_matrix=None, n_steps=1):
-        """When obsop (H) is linear"""
-        if H is None:
+        def _backprop_epoch(i, r0):
+            dr0 = grad(loss_func, argnums=0)(r0)
+            r0 -= self.learning_rate*dr0
+            return r0
+
+        return _backprop_epoch
+
+    def _cycle_obsop(self, xb, obs_values, obs_loc_indices, obs_error_sd,
+                     H=None, h=None, R=None, B=None, time_sel_matrix=None, n_steps=1):
+        if H is None and h is None:
             if self.H is None:
-                H = self._calc_default_H(obs_vec)
+                if self.h is None:
+                    H = self._calc_default_H(obs_values, obs_loc_indices)
+                else:
+                    h = self.h
             else:
                 H = self.H
         if R is None:
             if self.R is None:
-                R = self._calc_default_R(obs_vec)
+                R = self._calc_default_R(obs_values, obs_error_sd)
             else:
                 R = self.R
         if B is None:
@@ -132,19 +118,41 @@ class Backprop4D(dacycler.DACycler):
             else:
                 B = self.B
 
-        r0 = forecast.values
-        loss_func = self._make_loss(obs_vec.values, H, B, R, time_sel_matrix,
+        r0 = xb
+        loss_func = self._make_loss(obs_values, H, B, R, time_sel_matrix,
                                     rb0=r0, n_steps=n_steps)
-        for e in range(self.num_epochs):
-            epoch_loss, dr0 = value_and_grad(loss_func, argnums=0)(r0)
-            print(e, epoch_loss)
-            r0 -= self.learning_rate*dr0
+        backprop_epoch_func = self._make_backprop_epoch(loss_func)
+        r0 = jax.lax.fori_loop(0, self.num_epochs, backprop_epoch_func, r0)
 
         ra = self.step_forecast(
                 vector.StateVector(values=r0, store_as_jax=True),
                 n_steps=n_steps)
 
         return ra, None
+
+    def step_cycle(self, xb, yo, H=None, h=None, R=None, B=None, n_steps=1,
+                   obs_window_indices=[0]):
+        """Perform one step of DA Cycle
+
+        Args:
+            xb:
+            yo:
+            H
+
+
+        Returns:
+            vector.StateVector containing analysis results
+
+        """
+        time_sel_matrix = self._calc_time_sel_matrix(obs_window_indices,
+                                                     n_steps)
+        if H is not None or h is None:
+            return self._cycle_obsop(
+                    xb.values, yo.values, yo.location_indices, yo.error_sd,
+                    H, R, B, time_sel_matrix=time_sel_matrix, n_steps=n_steps)
+        else:
+            return self._cycle_obsop(
+                    xb, yo, h, R, B, time_sel_matrix=time_sel_matrix, n_steps=n_steps)
 
     def step_forecast(self, xa, n_steps=1):
         if 'n_steps' in inspect.getfullargspec(self.model_obj.forecast).args:
@@ -160,20 +168,45 @@ class Backprop4D(dacycler.DACycler):
                     out.append(xi)
                 return vector.StateVector(jnp.vstack(xi), store_as_jax=True)
 
+    def _cycle_and_forecast(self, state_obs_tuple, filtered_idx):
+        cur_state_vals = state_obs_tuple[0]
+        obs_vals = state_obs_tuple[1]
+        obs_times = state_obs_tuple[2]
+        obs_loc_indices = state_obs_tuple[3]
+        obs_error_sd = state_obs_tuple[4]
+
+        cur_obs_vals = jax.lax.dynamic_slice_in_dim(obs_vals, filtered_idx[0],
+                                                    len(filtered_idx))
+        cur_obs_loc_indices = jax.lax.dynamic_slice_in_dim(obs_loc_indices,
+                                                           filtered_idx[0],
+                                                           len(filtered_idx))
+        analysis, kh = self.step_cycle(
+                vector.StateVector(values=cur_state_vals, store_as_jax=True),
+                vector.ObsVector(values=cur_obs_vals,
+                                 location_indices=cur_obs_loc_indices,
+                                 error_sd=obs_error_sd,
+                                 store_as_jax=True),
+                n_steps=self.steps_per_window,
+                obs_window_indices=self.obs_window_indices)
+
+        return (analysis.values[-1], obs_vals, obs_times, obs_loc_indices,
+                obs_error_sd), analysis.values[:-1]
+
     def cycle(self,
               input_state,
               start_time,
               obs_vector,
+              obs_error_sd,
               timesteps,
               analysis_window,
-              obs_window_indices=[0],
-              steps_per_window=1,
               analysis_time_in_window=None):
         """Perform DA cycle repeatedly, including analysis and forecast
 
         Args:
             input_state (vector.StateVector): Input state.
             obs_vector (vector.ObsVector): Observations vector.
+            obs_error_sd (float): Standard deviation of observation error.
+                Typically not known, so provide a best-guess.
             start_time (float or datetime-like): Starting time.
             timesteps (int): Number of timesteps, in model time.
             analysis_window (float): Time window from which to gather
@@ -189,30 +222,32 @@ class Backprop4D(dacycler.DACycler):
 
         if analysis_time_in_window is None:
             analysis_time_in_window = analysis_window/2
-        # For storing outputs
-        all_analyses = []
-        all_times = []
-        cur_time = start_time + analysis_time_in_window
-        cur_state = input_state
 
-        for i in range(timesteps):
-            # 1. Filter observations to plus/minus 0.1 from that time
-            obs_vec_timefilt = obs_vector.filter_times(
-                cur_time - analysis_window/2, cur_time + analysis_window/2)
+        # NOTE: This is clumsy, but it sets up jax.lax.scan, which is VERY fast
+        # Working on a better way of setting it up
+        all_times = (
+                jnp.repeat(start_time + analysis_time_in_window, timesteps)
+                + jnp.arange(0, timesteps*analysis_window,
+                             analysis_window)
+                     )
+        all_filtered_idx = jnp.stack([jnp.where(
+            # Greater than start of window
+            (obs_vector.times > cur_time - analysis_window/2)
+            # AND Less than end of window
+            * (obs_vector.times < cur_time + analysis_window/2)
+            # OR Equal to start of window end
+            + jnp.isclose(obs_vector.times, cur_time - analysis_window/2,
+                          rtol=0)
+            # OR Equal to end of window
+            + jnp.isclose(obs_vector.times, cur_time + analysis_window/2,
+                          rtol=0)
+            )[0] for cur_time in all_times])
 
-            if obs_vec_timefilt.values.shape[0] > 0:
-                # 2. Calculate analysis
-                analysis, kh = self.step_cycle(
-                        cur_state, obs_vec_timefilt,
-                        n_steps=steps_per_window,
-                        obs_window_indices=obs_window_indices)
-                # 3. Save outputs
-                print(analysis.values.shape)
-                all_analyses.append(analysis.values[:-1])
-                all_times.append(cur_time)
+        cur_state, all_values = jax.lax.scan(
+                self._cycle_and_forecast,
+                (input_state.values, obs_vector.values, obs_vector.times,
+                 obs_vector.location_indices, obs_error_sd),
+                all_filtered_idx)
 
-            cur_time += analysis_window
-            cur_state = analysis[-1]
-
-        return vector.StateVector(values=jnp.vstack(all_analyses),
+        return vector.StateVector(values=jnp.vstack(all_values),
                                   store_as_jax=True)
