4e40099572c33010dc0fb92489643f2def72caff
Kylen Solvik <kysolvik@gmail.com>
2024-03-25 20:51:47 -0600

    Remove jax hessian computation code

--- dabench/dacycler/_var4d_backprop.py
+++ dabench/dacycler/_var4d_backprop.py
@@ -140,13 +140,11 @@ class Var4DBackprop(dacycler.DACycler):
     def _make_backprop_epoch(self, loss_func, optimizer, hessian_inv):
 
         loss_value_grad = value_and_grad(loss_func, argnums=0)
-#         hessian = jax.hessian(loss_func, argnums=0)
 
         @jax.jit
         def _backprop_epoch(epoch_state_tuple, _):
             x0, xb0, init_loss, i, opt_state = epoch_state_tuple
             loss_val, dx0 = loss_value_grad(x0)
-#             hessian_inv = jscipy.linalg.inv(hessian(x0))
             dx0_hess = hessian_inv @ dx0
             init_loss = jax.lax.cond(
                     i==0,
