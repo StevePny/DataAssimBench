Fix calculation of model timesteps for 4dvar and backprop

dabench/dacycler/_var4d.py
@@ -79,8 +79,6 @@ class Var4D(dacycler.DACycler):
                          ensemble=False,
                          B=B, R=R, H=H, h=h)
 
-        self._model_timesteps = jnp.arange(self.steps_per_window)*self.delta_t
-
     def _calc_default_H(self, obs_values):
         H = jnp.zeros((obs_values[0].shape[0], self.system_dim),
                       dtype=int)
@@ -364,7 +362,9 @@ class Var4D(dacycler.DACycler):
         all_times = dac_utils._get_all_times(start_time, analysis_window,
                                              n_cycles)
 
-        self.steps_per_window = round(analysis_window/self.delta_t) + 1
+        if self.steps_per_window is None:
+            self.steps_per_window = round(analysis_window/self.delta_t) + 1
+        self._model_timesteps = jnp.arange(self.steps_per_window)*self.delta_t
 
 
         # Get the obs vectors for each analysis window