3fdfabd100d7ec6cc426e9a5c72c2b0b624527a9
Kylen Solvik <kysolvik@gmail.com>
2024-10-01 19:48:46 -0600

    XArray accessors for helper methods

---
+++ dabench/data/_xarray_accessor.py
@@ -0,0 +1,62 @@
+import xarray as xr
+import numpy as np
+import warnings
+
+
+def _check_split_lengths(xr_obj, split_lengths):
+    total_length = np.sum(split_lengths)
+    xr_timedim = xr_obj.sizes['time']
+    if xr_timedim < total_length:
+        warnings.warn("Specified split lengths ({}) exceed \n"
+                      "Xarray object's time dimension ({}).".format(
+                          split_lengths, xr_timedim
+                      ))
+    elif xr_timedim > total_length:
+        warnings.warn("Specified split lengths ({}) are shorter than "
+                      "Xarray object's time dimension ({}).".format(
+                          split_lengths, xr_timedim
+                      ))
+
+
+@xr.register_dataset_accessor('dabench')
+class DABenchDatasetAccessor:
+    """Helper methods for manipulating xarray Datasets"""
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def to_system(self):
+        if 'time' in self._obj.coords:
+            remaining_dim = ['time']
+        else:
+            remaining_dim = []
+        return self._obj.to_stacked_array('system', remaining_dim)
+
+    def split_train_val_test(self, split_lengths):
+        _check_split_lengths(self._obj, split_lengths)
+        out_ds = []
+        start_i = 0
+        for sl in split_lengths:
+            end_i = start_i + sl
+            out_ds.append(self._obj.isel(time=slice(start_i, end_i)))
+        return tuple(out_ds)
+
+
+@xr.register_dataarray_accessor('dabench')
+class DABenchDataArrayAccessor:
+    """Helper methods for manipulating xarray DataArrays"""
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def to_gridded(self):
+        return self._obj.to_unstacked_dataset('system')
+
+    def split_train_val_test(self, split_lengths):
+        _check_split_lengths(self._obj, split_lengths)
+        out_ds = []
+        start_i = 0
+        for sl in split_lengths:
+            end_i = start_i + sl
+            out_ds.append(self._obj.isel(time=slice(start_i, end_i)))
+        return tuple(out_ds)
+
+
