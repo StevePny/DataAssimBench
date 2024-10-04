d7db346471510d262ed6c9b6d94b5f3b233a2999
Kylen Solvik <kysolvik@gmail.com>
2024-10-04 16:18:24 -0600

    Partially working DA tests (3dVar working)

--- tests/dacycler_etkf_test.py
+++ tests/dacycler_etkf_test.py
@@ -11,19 +11,17 @@ key = jrand.PRNGKey(42)
 
 
 @pytest.fixture
-def lorenz96():
+def l96_nature_run():
     """Defines class Lorenz96 object for rest of tests."""
     l96 = dab.data.Lorenz96(system_dim=5, store_as_jax=True, delta_t=0.01)
-    l96.generate(n_steps=25)
-
-    return l96
+    return l96.generate(n_steps=25)
 
 @pytest.fixture
-def obs_vec_l96(lorenz96):
+def obs_vec_l96(l96_nature_run):
     """Generate observations for rest of tests."""
     obs_l96 = dab.observer.Observer(
-        lorenz96,
-        time_indices=np.arange(0, 25, 5),
+        l96_nature_run,
+        times=l96_nature_run['time'].data[np.arange(0, 25, 5)],
         random_location_count=3,
         error_bias=0.1,
         error_sd=1.0,
@@ -36,18 +34,14 @@ def obs_vec_l96(lorenz96):
 
 @pytest.fixture
 def l96_fc_model():
-    model_l96 = dab.data.Lorenz96(system_dim=5, store_as_jax=True)
+    model_l96 = dab.data.Lorenz96(system_dim=5, store_as_jax=True, delta_t=0.01)
 
-    class L96Model(dab.model.Model):                                                                      
+    class L96Model(dab.model.Model):                                                                       
         """Defines model wrapper for Lorenz96 to test forecasting."""
         def forecast(self, state_vec, n_steps):
-            self.model_obj.generate(x0=state_vec.values, n_steps=n_steps)
-            new_vals = self.model_obj.values[:n_steps]
+            new_vec = self.model_obj.generate(x0=state_vec['x'].data, n_steps=n_steps)
 
-            new_vec = dab.vector.StateVector(values=new_vals,
-                                             store_as_jax=True)
-
-            return new_vec
+            return new_vec.isel(time=-1), new_vec
 
     return L96Model(model_obj=model_l96)
 
@@ -61,14 +55,14 @@ def etkf_cycler(l96_fc_model):
     
     return dc
 
-def test_etkf_l96(lorenz96, obs_vec_l96, etkf_cycler):
-
+def test_etkf_l96(l96_nature_run, obs_vec_l96, etkf_cycler):
     cur_tstep=10
     init_noise = jrand.normal(key, shape=(8, 5))
-    init_state = dab.vector.StateVector(
-        values=lorenz96.values[cur_tstep] + init_noise,
-        store_as_jax=True)
-    start_time = lorenz96.times[cur_tstep]
+    init_state = l96_nature_run.isel(time=cur_tstep)
+    init_state = init_state.assign(
+        x=(['ensemble','index'], init_state['x'].data + init_noise)
+    )
+    start_time = init_state['time'].data
 
     out_sv = etkf_cycler.cycle(
         input_state=init_state,
@@ -79,28 +73,30 @@ def test_etkf_l96(lorenz96, obs_vec_l96, etkf_cycler):
         n_cycles=10,
         return_forecast=True
         )
+    out_sv = out_sv.stack(time=['cycle','cycle_timestep']).transpose('time',...)
 
-    out_sv_mean = np.mean(out_sv.values, axis=1)
 
-    assert out_sv.values.shape == (100, 8, 5)
-    assert out_sv_mean.shape == (100, 5)
+    out_sv_mean = out_sv.mean(dim='ensemble')
+
+    assert out_sv['x'].shape == (100, 8, 5)
+    assert out_sv_mean['x'].shape == (100, 5)
     # Check that ensemble members are different
     assert not jnp.allclose(
-        out_sv.values[-1, 1, :],
-        out_sv.values[-1, 0, :],
+        out_sv['x'].values[-1, 1, :],
+        out_sv['x'].values[-1, 0, :],
     )
     # Check first cycle against presaved results
     assert jnp.allclose(
-        out_sv.values[0, 0, :],
+        out_sv['x'].values[0, 0, :],
         jnp.array([-0.85402591, 1.03480315, 0.51005132, 6.61546551, 8.1166806])
     )
     # Check last cycle against presaved results
     assert jnp.allclose(
-        out_sv.values[-1, 0, :],
+        out_sv['x'].values[-1, 0, :],
         jnp.array([0.66697948, 3.15465627, 5.39288975, -4.96130847, 2.17202611])
     )
     # Check mean against presaved results
     assert jnp.allclose(
-        out_sv_mean[-1, :],
+        out_sv_mean['x'].values[-1, :],
         jnp.array([1.45024252, 3.81627191, 5.4507981, 1.21646539, 0.09439264])
     )
--- tests/dacycler_var4d_var4dbp_test.py
+++ tests/dacycler_var4d_var4dbp_test.py
@@ -10,19 +10,17 @@ key = jrand.PRNGKey(42)
 
 
 @pytest.fixture
-def lorenz96():
+def l96_nature_run():
     """Defines class Lorenz96 object for rest of tests."""
     l96 = dab.data.Lorenz96(system_dim=6, store_as_jax=True, delta_t=0.01)
-    l96.generate(n_steps=120)
-
-    return l96
+    return l96.generate(n_steps=120)
 
 @pytest.fixture
-def obs_vec_l96(lorenz96):
+def obs_vec_l96(l96_nature_run):
     """Generate observations for rest of tests."""
     obs_l96 = dab.observer.Observer(
-        lorenz96, 
-        time_indices=jnp.arange(0, 120, 5),
+        l96_nature_run, 
+        times=l96_nature_run['time'].data[jnp.arange(0, 120, 5)],
         random_location_count = 3,
         error_bias = 0.0,
         error_sd = 0.3,
@@ -35,34 +33,34 @@ def obs_vec_l96(lorenz96):
 
 @pytest.fixture
 def l96_fc_model():
-    model_l96 = dab.data.Lorenz96(system_dim=6, store_as_jax=True)
+    model_l96 = dab.data.Lorenz96(system_dim=6, store_as_jax=True, delta_t=0.01)
 
     class L96Model(dab.model.Model):                                                                       
         """Defines model wrapper for Lorenz96 to test forecasting."""
         def forecast(self, state_vec, n_steps):
-            # NOTE: n_steps = 2 because the initial state counts as a "step"
-            self.model_obj.generate(x0=state_vec.values, n_steps=n_steps)
-            new_vals = self.model_obj.values
+            new_vec = self.model_obj.generate(x0=state_vec['x'].data, n_steps=n_steps)
 
-            new_vec = dab.vector.StateVector(values=new_vals, store_as_jax=True)
+            return new_vec.isel(time=-1), new_vec
 
-            return new_vec
         def compute_tlm(self, state_vec, n_steps):
-            """Compute TLM"""
-            M = self.model_obj.generate(n_steps=n_steps, x0=state_vec.values,
-                                        return_tlm=True)
-            return M, self.model_obj.values
+            x, M  = self.model_obj.generate(n_steps=n_steps, x0=state_vec['x'].data,
+                                            return_tlm=True)
+            return x, M
 
     return L96Model(model_obj=model_l96)
 
 @pytest.fixture
 def var4d_cycler(l96_fc_model):
+    B = jnp.identity(6)*0.05
+    R = jnp.identity(3) * (0.3**2)
     dc = dab.dacycler.Var4D(
         system_dim=6,
         delta_t=0.01,
         model_obj=l96_fc_model,
         obs_window_indices=[0,5, 10],
-        steps_per_window=11
+        steps_per_window=11,
+        B=B,
+        R=R
     )
     
     return dc
@@ -70,7 +68,8 @@ def var4d_cycler(l96_fc_model):
 
 @pytest.fixture
 def var4d_backprop_cycler(l96_fc_model):
-    B = jnp.identity(6)*0.05
+    B = jnp.identity(6)
+    R = jnp.identity(3) * ((0.3*1.5)**2)
     dc = dab.dacycler.Var4DBackprop(
         system_dim=6,
         delta_t=0.01,
@@ -79,18 +78,17 @@ def var4d_backprop_cycler(l96_fc_model):
         steps_per_window=11,
         learning_rate=0.1,
         lr_decay=0.5,
-        B=B
+        B=B,
+        R=R
     )
     
     return dc
 
-def test_var4d_l96(lorenz96, obs_vec_l96, var4d_cycler):
+def test_var4d_l96(l96_nature_run, obs_vec_l96, var4d_cycler):
     """Test 4D-Var cycler"""
     init_noise = jrand.normal(key, shape=(6,))
-    init_state = dab.vector.StateVector(
-        values=lorenz96.values[0] + init_noise,
-        store_as_jax=True)
-    start_time = lorenz96.times[0]
+    init_state = l96_nature_run + init_noise
+    start_time = l96_nature_run['time'].data[0]
 
     out_sv = var4d_cycler.cycle(
         input_state = init_state,
@@ -100,33 +98,32 @@ def test_var4d_l96(lorenz96, obs_vec_l96, var4d_cycler):
         n_cycles=10,  
         analysis_window=0.1,
         return_forecast=True)
+    out_sv = out_sv.stack(time=['cycle', 'cycle_timestep']).transpose('time', ...)
 
-    assert out_sv.values.shape == (100, 6)
+    assert out_sv['x'].shape == (100, 6)
 
     # Check that timeseries is evolving
     assert not jnp.allclose(
-        out_sv.values[0,:], 
-        out_sv.values[5,:], 
+        out_sv['x'].values[0,:], 
+        out_sv['x'].values[5,:], 
     )
     # Check against presaved results
     assert jnp.allclose(
-        out_sv.values[0,:], 
+        out_sv['x'].values[0,:], 
         jnp.array([4.5784335 , 10.70937771,  3.97859892,  0.25609285, -1.89681598,
                    -1.34747704])
     )
     assert jnp.allclose(
-        out_sv.values[-1,:],
+        out_sv['x'].values[-1,:],
         jnp.array([-2.32350141,  2.66564733,  9.1592932 ,  0.26887161, -2.72295144,
                     1.24513147])
     )
 
-def test_var4d_backprop_l96(lorenz96, obs_vec_l96, var4d_backprop_cycler):
+def test_var4d_backprop_l96(l96_nature_run, obs_vec_l96, var4d_backprop_cycler):
     """Test 4DVar-Backprop cycler"""
     init_noise = jrand.normal(key, shape=(6,))
-    init_state = dab.vector.StateVector(
-        values=lorenz96.values[0] + init_noise,
-        store_as_jax=True)
-    start_time = lorenz96.times[0]
+    init_state = l96_nature_run.isel(time=0) + init_noise
+    start_time = l96_nature_run['time'].data[0]
 
     out_sv = var4d_backprop_cycler.cycle(
         input_state = init_state,
@@ -136,22 +133,23 @@ def test_var4d_backprop_l96(lorenz96, obs_vec_l96, var4d_backprop_cycler):
         n_cycles=10,  
         analysis_window=0.1,
         return_forecast=True)
+    out_sv = out_sv.stack(time=['cycle', 'cycle_timestep']).transpose('time', ...)
 
-    assert out_sv.values.shape == (100, 6)
+    assert out_sv['x'].shape == (100, 6)
 
     # Check that timeseries is evolving
     assert not jnp.allclose(
-        out_sv.values[0,:], 
-        out_sv.values[5,:], 
+        out_sv['x'].values[0,:], 
+        out_sv['x'].values[5,:], 
     )
     # Check against presaved results
     assert jnp.allclose(
-        out_sv.values[0,:], 
+        out_sv['x'].values[0,:], 
         jnp.array([4.53548062, 9.00637144, 3.07940726, 3.24252952, -2.77042587,
                    -2.01121753])
     )
     assert jnp.allclose(
-        out_sv.values[-1,:],
+        out_sv['x'].values[-1,:],
         jnp.array([3.91514756,  6.5823489 , -1.60393758, -2.85701674, -0.5386405,
                    0.11637277])
     )
